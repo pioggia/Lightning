1caa7f236633e77fe1fd92e2953bce025352f171
unknown <raszeto@697-334-Harlond.sf.k12.internal>
2014-09-11 11:03:25 -0700

    Lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,36 @@
+int x=250;
+int y=0;
+int x2=250;
+int y2=0;
 void setup()
 {
-  size(300,300);
+	size(500, 500);
+	background(#1E262D);
+	strokeWeight(3);
 }
 void draw()
 {
-
+	if (keyPressed==true && key=='a')
+	{
+	background(#1E262D, 0.5);
+	colorMode(HSB);
+	stroke(((int)(Math.random()*30)+110), ((int)(Math.random()*30)+100), ((int)(Math.random()*20)+200));
+	while(y<500)
+	{
+		x2=x+((int)(Math.random()*11)-5);
+		y2=y+(int)(Math.random()*10);
+		line(x, y, x2, y2);
+		x=x2;
+		y=y2;
+	}
 }
-void mousePressed()
+else if (keyPressed==false)
 {
-
+	background(#1E262D, 0.5);
+}
+x=250;
+		y=0;
+		x2=250;
+		y2=0;
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,40 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int x=250;
+int y=0;
+int x2=250;
+int y2=0;
 public void setup()
 {
-  size(300,300);
-  noLoop();
+	size(500, 500);
+	background(0xff1E262D);
+	strokeWeight(3);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	if (keyPressed==true && key=='a')
+	{
+	background(0xff1E262D, 0.5f);
+	colorMode(HSB);
+	stroke(((int)(Math.random()*30)+110), ((int)(Math.random()*30)+100), ((int)(Math.random()*20)+200));
+	while(y<500)
+	{
+		x2=x+((int)(Math.random()*11)-5);
+		y2=y+(int)(Math.random()*10);
+		line(x, y, x2, y2);
+		x=x2;
+		y=y2;
+	}
 }
-public void mousePressed()
+else if (keyPressed==false)
 {
-  redraw();
+	background(0xff1E262D, 0.5f);
+}
+x=250;
+		y=0;
+		x2=250;
+		y2=0;
 }
 
   static public void main(String[] passedArgs) {
